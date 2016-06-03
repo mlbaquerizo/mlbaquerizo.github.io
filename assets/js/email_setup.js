15dc341c6ca7e10d6d686b6a2abb4edbe5e873cd
Matthew Baquerizo <mlbaquerizo47@gmail.com>
2016-06-03 16:20:47 -0400

    Add email form to contact div

---
+++ assets/js/email_setup.js
@@ -0,0 +1,88 @@
+function formHasEmptyFields() {
+  var errors = false;
+  $('.email-form-input').trigger('blur');
+    if(emptyFieldVal($('.email-form-input').val())){
+      errors = true;
+    } else {
+      errors = false;
+    }
+  return errors;
+}
+
+function formHasIncorrectEmail() {
+  var errors = false;
+  $('#email').trigger('blur');
+    if(!emailVal($('#email').val())){
+      errors = true;
+    } else {
+      errors = false;
+    }
+  return errors;
+}
+
+function emptyFieldVal(fieldVal) {
+  return fieldVal.trim() === ''
+}
+
+function emailVal(email) {
+  var emailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return emailRegex.test(email);
+}
+
+// get all data in form and return object
+function getFormData() {
+  var elements = document.getElementById("contact-form").elements; // all form elements
+  var fields = Object.keys(elements).map(function(k) {
+    if(elements[k].name !== undefined) {
+      return elements[k].name;
+    }
+  })
+  fields = fields.filter(function(item, pos, self) {
+    return self.indexOf(item) == pos && item;
+  });
+  var data = {};
+  fields.forEach(function(k){
+    data[k] = elements[k].value;
+  });
+  console.log(data);
+  return data;
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  // get the values submitted in the form
+  var data = getFormData();
+  // if email is not valid show error
+  if( formHasEmptyFields() || formHasIncorrectEmail() ) {
+    var message = 'Hi. Your message could not be received. Please try again.'
+    document.getElementById('after-submit').style.display = 'block';
+    document.getElementById('after-submit').innerHTML = message
+    return false;
+  } else {
+    var url = event.target.action;
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    // xhr.withCredentials = true;
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        console.log( xhr.status, xhr.statusText )
+        console.log(xhr.responseText);
+        var message = 'Your message has been received! I\'ll get back to you as soon as I can!'
+        document.getElementById('after-submit').style.display = 'block';
+        document.getElementById('after-submit').innerHTML = message
+        return;
+    };
+    // url encode form data for sending as post data
+    var encoded = Object.keys(data).map(function(k) {
+        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+    }).join('&')
+    xhr.send(encoded);
+  }
+}
+function loaded() {
+  console.log('contact form submission handler loaded successfully');
+  // bind to the submit event of our form
+  var form = document.getElementById('contact-form');
+  form.addEventListener("submit", handleFormSubmit, false);
+};
+document.addEventListener('DOMContentLoaded', loaded, false);
--- assets/js/home.js
+++ assets/js/home.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
 
-  $aboutButton = $('button[name="about"]')
-  $aboutDiv = $('div[id="content-about"]')
-  $workButton = $('button[name="work"]')
-  $workDiv = $('div[id="content-work"]')
-  $contactButton = $('button[name="contact"]')
-  $contactDiv = $('div[id="content-contact"]')
+  var $aboutButton = $('button[name="about"]')
+  var $aboutDiv = $('div[id="content-about"]')
+  var $workButton = $('button[name="work"]')
+  var $workDiv = $('div[id="content-work"]')
+  var $contactButton = $('button[name="contact"]')
+  var $contactDiv = $('div[id="content-contact"]')
+
+  $('nav').show("drop", 500);
 
   $aboutButton.hover(function(){
     $(this).css("border-bottom", "4px solid #3BC094")
@@ -14,7 +16,7 @@ $(document).ready(function(){
   })
 
   $aboutButton.click(function(){
-    $aboutDiv.toggle();
+    $aboutDiv.fadeToggle(400);
     $workDiv.hide();
     $contactDiv.hide();
   })
@@ -26,7 +28,7 @@ $(document).ready(function(){
   })
 
   $workButton.click(function(){
-    $workDiv.toggle();
+    $workDiv.fadeToggle(400);
     $aboutDiv.hide();
     $contactDiv.hide();
   })
@@ -38,9 +40,30 @@ $(document).ready(function(){
   })
 
   $contactButton.click(function(){
-    $contactDiv.toggle();
+    $contactDiv.fadeToggle(400);
     $workDiv.hide();
     $aboutDiv.hide();
   })
 
+  $('.email-form-input').on('blur', function(){
+    var $errorDiv = $(this).parent().find('span[id=' + $(this).prop('name') + '-validation]');
+    if(emptyFieldVal($(this).val())){
+      $errorDiv.html('required field');
+      $errorDiv.css('display', 'block');
+    } else {
+      $errorDiv.html('');
+      $errorDiv.css('display', 'none');
+    }
+  })
+
+  $('#email').on('blur', function(){
+    var $errorDiv = $(this).parent().find('span[id=' + $(this).prop('name') + '-validation]');
+    if(!emailVal($(this).val())){
+      $errorDiv.html('please enter a valid email address')
+      $errorDiv.css('display', 'block');
+    } else {
+      $errorDiv.html('');
+      $errorDiv.css('display', 'none');
+    }
+  })
 })
