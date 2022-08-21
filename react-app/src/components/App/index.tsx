42549e5e97c07a50669240daa34700dd2b6cbf8e
Matthew Baquerizo <mlbaquerizo47@gmail.com>
2022-08-20 21:50:14 -0400

    Use react-scroll-fade from the registry

--- react-app/src/components/App/index.tsx
+++ react-app/src/components/App/index.tsx
@@ -1,17 +1,8 @@
-import { useRef } from 'react';
-// import Typist from 'react-typist';
-import ImageWithFadein from '../ImageWithFadein';
-import { useIntersectionObserver } from '../../hooks';
+import { ImageWithFadein } from '@concarne/react-scroll-fade';
 import bonfireImgSrc from '../../assets/bonfire_v2.gif';
-import sittingImgSrc from '../../assets/sitting.png';
 import styles from './App.module.css';
 
 const App = () => {
-
-  const typistContainer1 = useRef(null);
-  const isTypistContainer1InView = useIntersectionObserver(typistContainer1);
-
-  console.log(isTypistContainer1InView);
 
   return (
     <div className={styles.app}>
@@ -19,28 +10,11 @@ const App = () => {
         <h1>MATTHEW BAQUERIZO</h1>
       </header>
       <main>
-        <div ref={typistContainer1} className={styles.typistContainer}>
-          {/* <Typist
-            cursor={{ show: false }}
-            avgTypingDelay={100}
-          >
-            <p>hello.</p>
-            <Typist.Delay ms={600} />
-            <p>what is going on?</p>
-          </Typist> */}
-        </div>
         <ImageWithFadein
           src={bonfireImgSrc}
           centerOffset={-5}
           scrollTo={250}
           imageTop={80}
-        />
-        <ImageWithFadein
-          src={sittingImgSrc}
-          width={200}
-          centerOffset={15}
-          scrollTo={350}
-          imageTop={105}
         />
       </main>
 
--- react-app/src/hooks/useIntersectionObserver/index.ts
+++ react-app/src/hooks/useIntersectionObserver/index.ts
@@ -1,14 +1,20 @@
 import { useEffect, useMemo, useState, RefObject } from 'react';
 
-const useIntersectionObserver = (ref: RefObject<Element|null>): boolean => {
+const useIntersectionObserver = (
+  ref: RefObject<Element|null>,
+  options?: IntersectionObserverInit
+): boolean => {
 
   const [isIntersecting, setIntersecting] = useState<boolean>(false);
 
   const observer = useMemo(() => {
     return new IntersectionObserver(([entry]) => {
+      // console.log("ENTRY:::::", entry);
       return setIntersecting(entry.isIntersecting)
-    });
-  }, []);
+    }, options);
+  }, [options]);
+
+  // console.log("OBSERVER:::::", observer);
 
   useEffect(() => {
     if(ref?.current) {
@@ -18,7 +24,7 @@ const useIntersectionObserver = (ref: RefObject<Element|null>): boolean => {
     return () => {
       observer.disconnect();
     }
-  }, []);
+  }, [ref, observer]);
 
   return isIntersecting;
 }
